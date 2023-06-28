dfec62ce4502978eb7f982d5afb724074dc04624
sofikin <sofia.kalpin@khk.ee>
2023-06-29 00:54:06 +0300

    Integrate route handling

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const routes = require('./routes');
 const app = express();
 app.set('view engine', 'ejs');
 
@@ -7,3 +8,4 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000, () => console.log('Server started on port 3000'));
+app.use('/', routes);
