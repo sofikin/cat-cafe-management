e78478e66ff446d6a07a891be951ea3312ed5860
sofikin <sofia.kalpin@khk.ee>
2023-06-29 20:35:57 +0300

    Implement routes and handlers for displaying menu

--- routes.js
+++ routes.js
@@ -14,20 +14,25 @@ router.post('/submit-order', (req, res) => {
 // Route for displaying menu and pricing information
 router.get('/menu', (req, res) => {
     // Logic to retrieve menu and pricing information
-    // ...
+    const menuItems = [
+        {name: 'Cafe Latte', category: 'beverage', price: 3.50},
+        {name: 'Cat-puccino', category: 'beverage', price: 4.00},
+        {name: 'Red Velvet Cake', category: 'food', price: 5.00},
+        {name: 'Salmon Sandwich', category: 'food', price: 6.50},
+    ];
     // Render the menu view and pass the data
-    res.render('menu', {menuItems, prices});
+    res.render('menu', {menuItems});
 });
 
 // Route for generating receipts
 router.get('/generate-receipt/:orderId', (req, res) => {
-    const { orderId } = req.params;
+    const {orderId} = req.params;
 
     // Logic to fetch order details and customer information from the database
     // ...
 
     // Render the receipt view and pass the data
-    res.render('receipt', { orderId, orderDetails, customerInfo });
+    res.render('receipt', {orderId, orderDetails, customerInfo});
 });
 
 module.exports = router;
